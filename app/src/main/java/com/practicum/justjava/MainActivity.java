6862d43815b1bade35743e4547a56b6e188278fc
Avenwes <anavas@gmail.com>
2022-10-31 12:43:09 +0500

    ver 0.2 (linear)

--- app/src/main/java/com/practicum/justjava/MainActivity.java
+++ app/src/main/java/com/practicum/justjava/MainActivity.java
@@ -23,19 +23,17 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder (View view){
-        int numberOfCoffees = 2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        displayPrice(quantity * 5);
     }
 
+    int quantity = 2;
+
     public void increment(View view) {
-        int quantity = 3;
-        display(quantity);
+        display(++quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
-        display(quantity);
+        display(--quantity);
     }
 
     private void display(int number) {
